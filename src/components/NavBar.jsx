fa45313a273acadf0ea2af35c68d9f147c548aed
rofaba <rodrigofaure@gmail.com>
2022-06-01 18:43:27 -0400

    entregable dos

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../images/logo-small.jpeg'
+import '../styles/navbar.css'
+
+function NavBar() {
+
+  return (
+    <div className = 'contenedor' >
+      
+      <img class='logo' src={logo} alt='logo app' />
+      
+      <ul class = 'barra-navegacion' >
+        <a href = 'https://es.stackoverflow.com/' target ='_blank'> Inicio </a> 
+        <a href = 'https://www.codewars.com/' target ='_blank' > Nosotros </a> 
+        <a href = 'https://www.algoexpert.io/product' target ='_blank' > Productos </a>
+        <a href = 'https://developer.mozilla.org/es/' target ='_blank' > Contacto </a> 
+      </ul >    
+    
+    </div>
+  );
+}
+export default NavBar;

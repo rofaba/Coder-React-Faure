43ade3b1b129ccd48f7cd18ce99bd4c0db8a1125
rofaba <rodrigofaure@gmail.com>
2022-06-02 08:57:45 -0400

    orden

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,15 +8,15 @@ function NavBar() {
     <div className = 'contenedor' >
       
       <img class='logo' src={logo} alt='logo app' />
+       <span> La Tienda sin Nombre </span>
+       {/* No tengo definida la tienda aún ni sus categorías */}
       
-      <ul class = 'barra-navegacion' >
-          {/* No tengo definida la tienda aún ni sus categorías */}
-        <span> La Tienda sin Nombre </span>
+      <div class = 'barra-navegacion' >
         <a href = 'https://es.stackoverflow.com/' target ='_blank'> Inicio </a> 
         <a href = 'https://www.codewars.com/' target ='_blank' > Nosotros </a> 
         <a href = 'https://www.algoexpert.io/product' target ='_blank' > Productos </a>
         <a href = 'https://developer.mozilla.org/es/' target ='_blank' > Contacto </a> 
-      </ul >    
+      </div >    
     
     </div>
   );
